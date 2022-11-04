831d5dbe043c2bf095e193d0132bc70abc487ceb
Mike Armstrong <mjarmstr@yahoo.com>
2022-11-03 22:30:59 -0500

    JWT added

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
 import cors from 'cors';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import config from 'config';
 import User from './models/User';
 
 // Initialize express application
@@ -17,21 +19,23 @@ app.use(
     cors({
         origin: 'http://localhost:3000'
     })
-)
+);
 
 // API endpoints
 /**
-* @route Get /
-* @desc Test endpoint
-*/
+ * @route GET /
+ * @desc Test endpoint
+ */
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
 
+app.get('/api/', (req, res) => res.send('http get request sent to api'));
+
 /**
-* @route POST api/users
-* @desc Register user
-*/
+ * @route POST api/users
+ * @desc Register user
+ */
 app.post(
     '/api/users',
     [
@@ -42,35 +46,51 @@ app.post(
     async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(422).json({ errors: errors.array() });
+        return res.status(422).json({ errors: errors.array() });
         } else {
-            const { name, email, password } = req.body;
-            try {
-                //Check if user exists
-                let user = await User.findOne({ email: email });
-                if (user) {
-                    return res
-                        .status(400)
-                        .json({ errors: [{ msg: 'User already exists' }] });
-                }
-
-                // Create a new user
-                user = new User({
-                    name: name,
-                    email: email,
-                    password: password
-                });
-
-                // Encrypt the password
-                const salt = await bcrypt.genSalt(10);
-                user.password = await bcrypt.hash(password, salt);
-
-                // Save to the db and return
-                await user.save();
-                res.send('User successfully registered');
-            } catch (error) {
-                res.status(500).send('Server error');
+        const { name, email, password } = req.body;
+        try {
+            // Check if user exists
+            let user = await User.findOne({ email: email });
+            if (user) {
+            return res
+                .status(400)
+                .json({ errors: [{ msg: 'User already exists' }] });
             }
+
+            // Create a new user
+            user = new User({
+            name: name,
+            email: email,
+            password: password
+            });
+
+            // Encrypt the password
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+
+            // Save to the db and return
+            await user.save();
+
+            // Generate and return a JWT token
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                { expiresIn: '10hr' },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token: token });
+                }
+            );
+        } catch (error) {
+            res.status(500).send('Server error');
+        }
         }
     }
 );
